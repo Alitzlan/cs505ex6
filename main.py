588c66d711a7db942e2816a373ba302818307c0a
Chi Yang <alitzlan@gmail.com>
2016-03-14 00:16:48 -0400

    add ping all test

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 """""""""""""""""""""""""""""""""""""""
-from h5py.h5s import NULL
 FILE: main.py
 PURPOSE: main program for every host
 AUTHOR: Chi Yang
@@ -7,36 +6,79 @@ AUTHOR: Chi Yang
 
 import sys
 import logging
+import socket
 from optparse import OptionParser
 from os import path
 
 from host import Host
 
+myid = None
+myname = None
+myport = None
+mysock = None
+
 def pingAll():
-    pass
+    if mysock == None:
+        logger.error("Socket is not initialized")
+        sys.exit()
+        
+    # broadcast
+    for host in HOSTS.itervalues():
+        mysock.sendto("Are you there?", (host.hostname, host.port))
+        
+    cnt = 0
+    while cnt < len(HOSTS):
+        data, addr = mysock.recvfrom(512)
+        print addr,":",data
+        if data == "Are you there?":
+            mysock.sendto("Yes, I am.", addr)
+        else:
+            cnt += 1
 
 def initSocket():
-    pass
+    global mysock
+    mysock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    mysock.bind((myname, myport))
 
 def parseHostfile():
-    global HOSTS
+    global HOSTS, myid, myname, myport
     HOSTS = dict()
     hostfile = open(HOSTFILE, "rb")
     for line in hostfile:
         hostinfo = line.rstrip("\r\n").split(" ")
+        
+        # check least number of arg
         if len(hostinfo) < 2:
-            logging.error("Not enough args in hostfile")
+            logger.error("Not enough args in hostfile")
             sys.exit()
         hostid = int(hostinfo[0])
         hostname = hostinfo[1]
+        
+        # autocomplete port
         if len(hostinfo) < 3:
-            logging.warning("Port for host id {0} not assigned, assign {1}".format(hostid, PORT))
+            logger.warning("Port for host id {0} not assigned, assign {1}".format(hostid, PORT))
             hostport = PORT
         else:
             hostport = int(hostinfo[2])
+            
+        
+        # check existence
         if HOSTS.has_key(hostid):
-            logging.error("Host id {0} already exists, abort".format(hostid))
+            logger.error("Host id {0} already exists, abort".format(hostid))
+            
+        # find me
+        if hostname == socket.gethostname() and hostport == PORT:
+            myid, myname, myport = hostid, hostname, hostport
+            logger.info("New Host: {0} {1} {2} *".format(hostid, hostname, hostport))
+        else:
+            logger.info("New Host: {0} {1} {2}".format(hostid, hostname, hostport))
+        
         HOSTS[hostid] = Host(hostid, hostname, hostport)
+        
+    # check result
+    if myid == None:
+        logger.error("I am not in the hostfile")
+        sys.exit()
 
 def parseOpt():
     global PORT, HOSTFILE, MAXCRASH
@@ -60,12 +102,22 @@ def parseOpt():
     if path.isfile(HOSTFILE):
         parseHostfile()
     else:
-        logging.error("Host file not exist")
+        logger.error("Host file not exist")
         sys.exit()
 
 def main():
+    # initial logger
+    global logger
+    logging.basicConfig()
+    logger = logging.getLogger("global")
+    logger.setLevel(logging.INFO)
+    
+    # initialization
     parseOpt()
+    initSocket()
     pingAll()
+    
+    
 
 if __name__ == "__main__":
     main()
