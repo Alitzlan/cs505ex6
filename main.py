237ce1166551fc16392c4e02fef35086247bcdbe
Chi Yang <yang838@xinu05.cs.purdue.edu>
2016-03-25 13:24:12 -0400

    update script and leader change

--- main.py
+++ main.py
@@ -39,15 +39,15 @@ LEADER_TIMEOUT = 0.25
 
 def pingAll():
     global mysock
-    
+
     if mysock == None:
         logerror(myid,"Socket is not initialized")
         sys.exit()
-        
+
     # broadcast
     for addr in IDLOOKUP.keys():
         mysock.sendto("Are you there?", addr)
-        
+
     pending = ADDRLOOKUP.keys()
     mysock.settimeout(0.5)
     starttime = time.time()
@@ -69,9 +69,9 @@ def pingAll():
             else:
                 logerror(myid,msg)
                 sys.exit()
-    
+
     mysock.settimeout(None)
-    
+
     if len(pending) == 0:
         loginfo(myid,"All ping success")
     else:
@@ -92,20 +92,20 @@ def parseHostfile():
     ADDRLOOKUP = dict()
     # get hostname by ip
     NAMELOOKUP = dict()
-    
+
     myip = socket.gethostbyname(socket.gethostname())
-    
+
     hostfile = open(HOSTFILE, "rb")
     for line in hostfile:
         hostinfo = line.rstrip("\r\n").split(" ")
-        
+
         # check least number of arg
         if len(hostinfo) < 2:
             logerror(myid,"Not enough args in hostfile")
             sys.exit()
         hostid = int(hostinfo[0])
         hostname = hostinfo[1]
-        
+
         try:
             hostip = socket.gethostbyname(hostname)
         except socket.gaierror, msg:
@@ -115,15 +115,15 @@ def parseHostfile():
             else:
                 logerror(myid,msg)
                 sys.exit()
-        
+
         # auto-complete port
         if len(hostinfo) < 3:
             logwarning(myid,"Port for host id {0} not assigned, assign {1}".format(hostid, PORT))
             hostport = PORT
         else:
             hostport = int(hostinfo[2])
-            
-        
+
+
         # check existence
         if ADDRLOOKUP.has_key(hostid):
             logerror(myid,"Host id {0} already exists, abort".format(hostid))
@@ -133,7 +133,7 @@ def parseHostfile():
             sys.exit()
         if not NAMELOOKUP.has_key(hostip):
             NAMELOOKUP[hostip] = hostname
-            
+
         # find me
         if (myip == hostip or hostip == "127.0.0.1") and hostport == PORT:
             myid, myname, myip, myport = hostid, hostname, hostip, hostport
@@ -141,10 +141,10 @@ def parseHostfile():
             loginfo(myid,"New Host: {0} {1} {2} *".format(hostid, hostname, hostport))
         else:
             loginfo(myid,"New Host: {0} {1} {2}".format(hostid, hostname, hostport))
-        
+
         ADDRLOOKUP[hostid] = (hostip, hostport)
         IDLOOKUP[(hostip, hostport)] = hostid
-        
+
     # check result
     if myid == None:
         logerror(myid,"I am not in the hostfile")
@@ -164,11 +164,11 @@ def parseOpt():
                       help="max crashes allowed, default 0",
                       nargs=1, type="int", default=0)
     options, args = parser.parse_args()
-    
+
     PORT = int(options.port)
     HOSTFILE = options.hostfile
     MAXCRASH = int(options.maxcrash)
-    
+
     if path.isfile(HOSTFILE):
         parseHostfile()
     else:
@@ -182,6 +182,7 @@ def followerHandle(data, addr):
         if msg.term > myterm:
             myterm = msg.term
             myleader = msg.id
+            leaderchange = True
         elif msg.term == myterm and myleader != msg.id:
             myterm = msg.term
             myleader = msg.id
@@ -193,14 +194,13 @@ def followerHandle(data, addr):
             myterm = msg.term
             myleader = msg.id
             mysock.sendto(MessageBody(MessageType.Vote, myterm, msg.id).toString(), addr)
-            leaderchange = True
         elif msg.term <= myterm:
             mysock.sendto(MessageBody(MessageType.Vote, myterm, myleader).toString(), addr)
     elif msg.type == MessageType.Vote:
         pass
 
 def followerLoop():
-    global myid, myname, myip, myport, myaddr, mysock, myterm, myvote, myleader
+    global myid, myname, myip, myport, myaddr, mysock, myterm, myvote, myleader, leaderchange
     mysock.settimeout(FOLLOWER_TIMEOUT)
     while(True):
         try:
@@ -211,6 +211,7 @@ def followerLoop():
             loginfo(myid,"timeout")
             if myleader != None:
                 print "[{0}] Node {1}: leader node {2} has crashed.".format(time.strftime("%H:%M:%S",time.localtime()), myid, myleader)
+            leaderchange = True
             return RaftState.Candidate
         except socket.error, msg:
             if msg[0] == 10054 and sys.platform == "win32":
@@ -219,7 +220,7 @@ def followerLoop():
             else:
                 logerror(myid,msg)
                 sys.exit()
-                
+
 def candidateLoop():
     global myid, myname, myip, myport, myaddr, mysock, myterm, myvote, myleader, living
     mysock.settimeout(CANDIDATE_TIMEOUT)
@@ -228,13 +229,13 @@ def candidateLoop():
     election_timeout = random.randrange(150, 300) / 1000.
     requestmsg = MessageBody(MessageType.RequestVote, myterm, myid)
     peerid = ADDRLOOKUP.keys()
-    
+
     # for counting
     if living == None:
         living = set(peerid)
     pending = set(living)
     cnt = Counter()
-    
+
     start = time.time()
     # broadcast request vote
     for id in peerid:
@@ -290,8 +291,8 @@ def candidateLoop():
                 logerror(myid,msg)
                 sys.exit()
     # election timeout
-    return RaftState.Candidate 
-                
+    return RaftState.Candidate
+
 def leaderLoop():
     global myid, myname, myip, myport, myaddr, mysock, myterm, myvote, myleader
     mysock.settimeout(LEADER_TIMEOUT)
@@ -300,7 +301,7 @@ def leaderLoop():
             data, addr = mysock.recvfrom(512)
             logdebug(myid, str(addr)+":"+data)
             msg = MessageBody.fromStr(data)
-            if msg.term > myterm: 
+            if msg.term > myterm:
                 followerHandle(data, addr)
                 return RaftMessage.Follower
         except socket.timeout, msg:
@@ -308,7 +309,7 @@ def leaderLoop():
             heartbeat = MessageBody()
             heartbeat.term = myterm
             heartbeat.id = myid
-            # broadcast heart_beat 
+            # broadcast heart_beat
             for id in ADDRLOOKUP.keys():
                 #don't send to self
                 if(id!=myid):
@@ -322,12 +323,10 @@ def leaderLoop():
                 sys.exit()
 
 def main():
-    
-    
     # initialization
     parseOpt()
     initSocket()
-    
+
     # state loop
     nextState = RaftState.Follower
     while(True):
